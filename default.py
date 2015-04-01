c5513bee0986cd2ea7a3624788b2da670526ea80
enen92 <92enen@gmail.com>
2015-04-01 15:58:58 +0100

    added atari2600, sega genesis and game gear

--- default.py
+++ default.py
@@ -18,6 +18,9 @@ from resources.downloadtools import *
 
 def main_menu():
 	addDir(translate(30001),'https://archive.org/details/softwarelibrary_msdos_games?&sort=-downloads&page=1',1, os.path.join(artfolder,'dosboxicon.png'),1,os.path.join(artfolder,'dosbox.jpg'))
+	addDir(translate(30014),'https://archive.org/details/sega_genesis_library?&sort=-downloads&page=1',1,'http://thegamesdb.net/banners/platform/consoleart/18.png',1,'http://thegamesdb.net/banners/platform/fanart/18-3.jpg')
+	addDir(translate(30015),'https://archive.org/details/gamegear_library?&sort=-downloads&page=1',1,'http://thegamesdb.net/banners/platform/consoleart/20.png',1,'http://thegamesdb.net/banners/platform/fanart/20-2.jpg')
+	addDir(translate(30016),'https://archive.org/details/atari_2600_library?&sort=-downloads&page=1',1,'http://psx-scene.com/forums/attachments/f190/35672d1339560883-%5Bps3%5D-emulator-downloads-w-complete-info-atari-2600.png',1,'http://thegamesdb.net/banners/platform/fanart/22-3.jpg')
 	
 
 def dos_box(url):
@@ -28,13 +31,29 @@ def dos_box(url):
 		matchgames = re.findall('<div class="item-ia"(.*?)<div class="C5"></div>',page_source,re.DOTALL)
 		totalmatch = len(matchgames)
 		if matchgames:
-			addDir(translate(30002),'gameurl',2,'',1,os.path.join(artfolder,'dosbox.jpg'))
+			
+			#add costumized search url
+			if '_msdos_games' in url: 
+				console_fanart = os.path.join(artfolder,'dosbox.jpg')
+				addDir(translate(30002),'https://archive.org/details/softwarelibrary_msdos_games?and[]=',2,'',1,os.path.join(artfolder,'dosbox.jpg'))
+			elif 'sega_genesis' in url:
+				console_fanart = 'http://thegamesdb.net/banners/platform/fanart/18-3.jpg'
+				addDir(translate(30002),'https://archive.org/details/sega_genesis_library?and[]=',2,'',1,console_fanart)
+			elif 'atari_2600' in url:
+				console_fanart = 'http://thegamesdb.net/banners/platform/fanart/22-3.jpg'
+				addDir(translate(30002),'https://archive.org/details/atari_2600_library?and[]=',2,'',1,console_fanart)
+			elif 'gamegear' in url:
+				console_fanart = 'http://thegamesdb.net/banners/platform/fanart/20-2.jpg'
+				addDir(translate(30002),'https://archive.org/details/gamegear_library?and[]=',2,'',1,console_fanart)
+			
+			year = 0
 			for gameinfo in matchgames:
 				title = re.compile('title=".+?">(.+?)</a>').findall(gameinfo)
 				if selfAddon.getSetting('img-resolution') == '0':
 					try: 
 						game_file = re.compile('<img class="item-img" source="(.+?)"').findall(gameinfo)[0].split('/')[-1]
-						iconimage = 'http://archive.org/serve/' + game_file + '/' + game_file.replace('msdos_','') + '_screenshot.gif'
+						iconimage = 'http://archive.org/serve/' + game_file + '/' + game_file.replace('msdos_','').replace('sg_','').replace('gg_','') + '_screenshot.gif'
+						if 'atari_2600' in iconimage: iconimage = iconimage.replace('.gif','.png')
 					except: iconimage = ''
 				else:
 					try: 
@@ -55,7 +74,10 @@ def dos_box(url):
 								if len(year_dig) > 1: year = year_dig[-1]
 								else: year = year_dig[0]
 					except:
-						year = ''
+						try:
+							year_str = re.compile('_(\d+)_').findall(gameplot)[-1]
+							if len(year_str) == 4 and year_str != 2600: year = int(year_str)
+						except: year = 0
 					
 					#Genre
 					try: genre = re.compile('Genre (.+?) .+?Description').findall(gameplot)[0].replace(',','')
@@ -72,9 +94,17 @@ def dos_box(url):
 					
 				except: 
 					gameplot = ''
-					year = ''
+					year = 0
 					genre = ''
 					author = ''
+					
+				#Put year to zero match year in plot
+				if year == 2600: year= 0
+				year_opt = re.compile('19(\d+)').findall(gameplot)
+				if year_opt:
+					year = int('19'+str(year_opt[0]))
+				
+					
 				if title:
 					infolab = { "Title": title[0],"Plot": gameplot, "Year":year, "Genre":genre, "Studio":author,"Director":author, "Writer":author}
 					addGame('[B]'+title[0]+'[/B]',gameurl,3,iconimage,totalmatch,iconimage,infolab)#os.path.join(artfolder,'dosbox.jpg'))
@@ -85,7 +115,7 @@ def dos_box(url):
 					cur_page = re.findall('&page=(\d+)',url)[0]
 					next_page = int(cur_page) + 1
 					url_next = url.replace('page='+str(cur_page),'page='+str(next_page))
-					addDir(translate(30003),url_next,1,'',1,os.path.join(artfolder,'dosbox.jpg'))
+					addDir(translate(30003),url_next,1,'',1,console_fanart)
 				except: pass
 			xbmcplugin.setContent(int(sys.argv[1]), 'movies')
 			xbmc.sleep(200)
@@ -105,18 +135,22 @@ def archive_resolver(url):
 	files = source["files"]
 	rom = ''
 	for _file_ in files:
-		if _file_["name"].endswith('.zip'):
+		if _file_["name"].endswith('.zip') or _file_["name"].endswith('.bin'):
 			rom = _file_["name"]
 			break
 		else: pass
 	if rom:
 		download_url = "http://"+d1+dire+'/'+rom
-		print download_url
 		if selfAddon.getSetting('download-folder') == '':
 			downloadPath = xbmcgui.Dialog().browse(int(3), 'Select Download Folder','myprograms')
 			selfAddon.setSetting('download-folder',downloadPath)
 		else: downloadPath = selfAddon.getSetting('download-folder')
-		destination_folder = os.path.join(downloadPath,'DOS')
+		
+		if 'sg_' in download_url: destination_folder = os.path.join(downloadPath,'genesis')
+		elif 'gg_' in download_url: destination_folder = os.path.join(downloadPath,'gamegear')
+		elif 'msdos_' in download_url: destination_folder = os.path.join(downloadPath,'DOS')
+		elif 'atari_2600_' in download_url: destination_folder = os.path.join(downloadPath,'atari2600')
+		
 		if not os.path.exists(destination_folder): xbmcvfs.mkdir(destination_folder)
 		
 		destination_file = os.path.join(destination_folder,rom)
@@ -139,13 +173,14 @@ def read_details(name,url):
 
 	
 	
-def archive_search():
+def archive_search(url):
+
 	keyb = xbmc.Keyboard('', translate(30008))
 	keyb.doModal()
 	if (keyb.isConfirmed()):
 		search = keyb.getText()
 		encode=urllib.quote(search)
-		urltmp = 'https://archive.org/details/softwarelibrary_msdos_games?and[]='+encode+'&page=1'
+		urltmp = url+encode+'&page=1'
 		dos_box(urltmp)
 
 
@@ -205,7 +240,7 @@ print ("iconimage: "+str(iconimage))
 
 if mode==None: main_menu()
 elif mode==1: dos_box(url)
-elif mode==2: archive_search()
+elif mode==2: archive_search(url)
 elif mode==3: archive_resolver(url)
 elif mode==4: read_details(name,url)
 
